71b71033fbacab87d14742b3107337bed8e45af3
PiesArentSquare <jjhh12@outlook.com>
2022-04-04 16:38:15 -0400

    separated 3dview into its own file

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,6 +4,8 @@ import toolbar from "./components/toolbar.js"
 
 import {pen_tool, eraser_tool, eyedropper_tool, fill_tool} from "./tools.js"
 
+import createSkinViewer from "./skin_viewer.js"
+
 const canvas = new pixelCanvas("#canvas", 8, 12)
 const picker = new colorPicker("#color-picker", c => canvas.currentColor = c.copy())
 
@@ -32,92 +34,5 @@ window.addEventListener("keydown", e => {
         tools.setActiveFromKeybind(e.code)
 })
 
-import * as THREE from 'three'
-import WebGL from 'three/examples/jsm/capabilities/WebGL'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-
-const threedview = <HTMLDivElement>document.querySelector("#threedview")
-
-if (WebGL.isWebGLAvailable()) {
-    const width = threedview.clientWidth
-    const height = threedview.clientHeight
-    
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
-    camera.translateZ(5)
-    
-    const renderer = new THREE.WebGLRenderer()
-    threedview.appendChild(renderer.domElement)
-    
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(width, height)
-
-    const controls = new OrbitControls(camera, renderer.domElement)
-    controls.enableDamping = true
-    controls.enablePan = false
-    controls.enableZoom = false
-
-    const steveURL = new URL('../assets/model/steve.glb', import.meta.url).href
-    const steveOuterURL = new URL('../assets/model/steveOuter.glb', import.meta.url).href
-    let steve: THREE.Group
-    let steveOuter: THREE.Group
-    const textureLoader = new THREE.TextureLoader()
-
-    const steveMaterial = new THREE.MeshStandardMaterial()
-    const steveOuterMaterial = new THREE.MeshStandardMaterial({transparent: true, side: THREE.DoubleSide})
-
-    const updateTexture = async (url: string) => {
-        let skin = await textureLoader.loadAsync(url)
-        skin.minFilter = THREE.NearestFilter
-        skin.magFilter = THREE.NearestFilter
-        skin.flipY = false
-        if (steveMaterial.map) {
-            steveMaterial.map = skin
-            steveOuterMaterial.map = skin
-        } else {
-            steveMaterial.map = skin
-            steveOuterMaterial.map = skin
-            steveMaterial.needsUpdate = true
-            steveOuterMaterial.needsUpdate = true
-        }
-    }
-
-    const load = async () => {
-        const modelLoader = new GLTFLoader()
-        steve = (await modelLoader.loadAsync(steveURL)).scene
-        steve.traverse(node => {
-            if (!(node instanceof THREE.Mesh)) return
-            node.material = steveMaterial
-        })
-        scene.add(steve)
-
-        steveOuter = (await modelLoader.loadAsync(steveOuterURL)).scene
-        steveOuter.traverse(node => {
-            if (!(node instanceof THREE.Mesh)) return
-            node.material = steveOuterMaterial
-        })
-        scene.add(steveOuter)
-    }
-    load()
-    
-    canvas.onUpdate = () => {
-        updateTexture(canvas.generateImageURL())
-    }
-    
-    // const pointLight = new THREE.PointLight(0xffffff)
-    // pointLight.position.set(5, 5, 5)
-    // scene.add(pointLight)
-    const ambientLight = new THREE.AmbientLight(0xffffff)
-    scene.add(ambientLight)
-    
-    function animate() {
-        requestAnimationFrame(animate)
-        controls.update()
-        renderer.render(scene, camera)
-    }
-    animate()
-} else {
-    const warning = WebGL.getWebGLErrorMessage()
-    threedview.appendChild(warning)
-}
+const threedview = createSkinViewer(<HTMLDivElement>document.querySelector("#threedview"), false)
+if (threedview) canvas.onUpdate = () => threedview.setTexture(canvas.generateImageURL())
---
+++ src/ts/skin_viewer.ts
@@ -0,0 +1,103 @@
+import {
+    Scene, WebGLRenderer, PerspectiveCamera,
+    Group, Mesh, MeshStandardMaterial,
+    AmbientLight,
+    TextureLoader,
+    DoubleSide, NearestFilter
+} from 'three'
+
+import WebGL from 'three/examples/jsm/capabilities/WebGL'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+export default function createSkinViewer(parent: HTMLElement, alex: boolean): skinViewer | void {
+    if (WebGL.isWebGLAvailable()) {
+        return new skinViewer(parent, alex)
+    } else {
+        const warning = WebGL.getWebGLErrorMessage()
+        parent.appendChild(warning)
+    }
+}
+
+const steveInnerURL = new URL('../assets/model/steve.glb', import.meta.url).href
+const steveOuterURL = new URL("../assets/model/steveOuter.glb", import.meta.url).href
+const alexInnerURL = new URL("../assets/model/alex.glb", import.meta.url).href
+const alexOuterURL = new URL("../assets/model/alexOuter.glb", import.meta.url).href
+
+export class skinViewer {
+    private scene = new Scene
+    private renderer = new WebGLRenderer
+    private camera: PerspectiveCamera
+
+    private innerModel: Group | undefined
+    private outerModel: Group | undefined
+    private innerMaterial = new MeshStandardMaterial
+    private outerMaterial = new MeshStandardMaterial({ transparent: true, side: DoubleSide })
+
+    private textureLoader = new TextureLoader
+    private modelLoader = new GLTFLoader
+
+    constructor(parent: HTMLElement, alex: boolean) {
+        const width = parent.clientWidth
+        const height = parent.clientHeight
+
+        this.camera = new PerspectiveCamera(75, width / height, 0.1, 1000)
+        this.camera.translateZ(5)
+        
+        this.renderer.setPixelRatio(window.devicePixelRatio)
+        this.renderer.setSize(width, height)
+
+        const controls = new OrbitControls(this.camera, this.renderer.domElement)
+        controls.enableDamping = true
+        controls.enablePan = false
+        controls.enableZoom = false
+
+        this.loadAssets(alex)
+
+        this.scene.add(new AmbientLight(0xffffff))
+
+        const animate = () => {
+            requestAnimationFrame(animate)
+            controls.update()
+            this.renderer.render(this.scene, this.camera)
+        }
+        animate()
+
+        parent.appendChild(this.renderer.domElement)
+    }
+
+    private async loadAssets(alex: boolean) {
+        const inner = alex ? alexInnerURL : steveInnerURL
+        const outer = alex ? alexOuterURL : steveOuterURL
+        this.innerModel = (await this.modelLoader.loadAsync(inner)).scene
+        this.outerModel = (await this.modelLoader.loadAsync(outer)).scene
+        
+        this.innerModel.traverse(node => {
+            if (!(node instanceof Mesh)) return
+            node.material = this.innerMaterial
+        })
+
+        this.outerModel.traverse(node => {
+            if (!(node instanceof Mesh)) return
+            node.material = this.outerMaterial
+        })
+
+        this.scene.add(this.innerModel, this.outerModel)
+    }
+
+    async setTexture(url: string) {
+        let skin = await this.textureLoader.loadAsync(url)
+        skin.minFilter = NearestFilter
+        skin.magFilter = NearestFilter
+        skin.flipY = false
+        if (this.innerMaterial.map) {
+            this.innerMaterial.map = skin
+            this.outerMaterial.map = skin
+        } else if (this.innerMaterial) {
+            this.innerMaterial.map = skin
+            this.outerMaterial.map = skin
+            this.innerMaterial.needsUpdate = true
+            this.outerMaterial.needsUpdate = true
+        }
+    }
+}
