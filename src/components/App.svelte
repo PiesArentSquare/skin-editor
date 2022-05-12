adcdb0425ba056efe96097b0aff514a7fe1ba3eb
PiesArentSquare <jjhh12@outlook.com>
2022-05-12 18:20:25 -0400

    added nav

---
+++ src/components/App.svelte
@@ -0,0 +1,7 @@
+<script lang=ts>
+    import Nav from './Nav.svelte'
+</script>
+
+<div class="container">
+    <Nav/>
+</div>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,44 @@
+<nav>
+    <div class="inner">
+        <ul>
+            <li id="new">new</li>
+            <li id="open">open</li>
+            <li id="save">save</li>
+        </ul>
+        <h1>untitled*</h1>
+    </div>
+</nav>
+
+<style lang="scss">
+    @use 'src/styles/common';
+
+    nav {
+        background-color: white;
+        padding: 0.5rem 2rem;
+        box-shadow: common.$shadow;
+        z-index: 1;
+        color: common.$secondary-color;
+    }
+
+    .inner {
+        position: relative;
+    }
+
+    ul {
+        display: flex;
+    }
+
+    li {
+        margin-right: 2rem;
+    }
+
+    h1 {
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        display: block;
+        font-size: 1rem;
+        font-weight: 400;
+    }
+</style>
